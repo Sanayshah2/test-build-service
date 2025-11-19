3d6c020abc650097e5fcd94db09e0ba8476c5212
Sanay Shah <sanayshah2@gmail.com>
2025-11-18 17:14:25 -0800

    Update text in main.ts for change testing

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more. Build from the git commit 10
+      Click on the Vite and TypeScript logos to learn more. Build from the git commit for william's change testing
     </p>
   </div>
 `
